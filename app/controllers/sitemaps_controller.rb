e731fae73bdc60bde3ab3654eb4b5c0b6e7fc02f
Mike Petersen <mike@odania-it.de>
2010-06-19 16:59:25 +0200

    Added Subwidgets

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,15 +1,14 @@
 class SitemapsController < ActionController::Base
   
-  def show
-    @widgets = SitemapWidget.find(:all)
-    @site = SitemapSetting.find(:first)
-    @static_links = SitemapStaticLink.find(:all)
-    respond_to do |format|
-      format.html
-      format.xml
-      format.css
-      format.xsl
-    end
-  end
-  
+	def show
+		@widgets      = SitemapWidget.find(:all, :conditions => "parent_widget_id IS NULL")
+		@site         = SitemapSetting.find(:first)
+		@static_links = SitemapStaticLink.find(:all)
+		respond_to do |format|
+			format.html
+			format.xml
+			format.css
+			format.xsl
+		end
+	end
 end
