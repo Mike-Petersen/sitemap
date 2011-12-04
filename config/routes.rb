e6f2596bc40f449ce511a73cfb771e2b3ea56f2c
Mike Petersen <mike@odania-it.de>
2011-12-04 23:02:10 +0100

    Updated sitemap for rails 3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Mime::Type.register_alias "text/xml", :xsl
 
-
-class << ActionController::Routing::Routes;self;end.class_eval do
-  define_method :clear!, lambda {}
-end
-
-ActionController::Routing::Routes.draw do |map|
-  map.resource :sitemap
-  map.resources :sitemap_settings
-  map.resources :sitemap_widgets
-  map.resources :sitemap_static_links
+Rails.application.routes.draw do
+  resource :sitemap
+  resources :sitemap_settings
+  resources :sitemap_widgets
+  resources :sitemap_static_links
 end
