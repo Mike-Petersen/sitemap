3e79baa80714fe8de57f4ecc7f7974d101f1908d
Mike Petersen <mike@odania-it.de>
2011-12-10 15:52:52 +0100

    Added caching to sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,8 +1,11 @@
 class SitemapsController < ActionController::Base
-  
+	caches_action :show, :expires_in => 1.hour, :race_condition_ttl => 1, :cache_path => Proc.new { |controller|
+		sitemaps_url(format: params[:format])
+	}
+
 	def show
-		@widgets      = SitemapWidget.find(:all, :conditions => "parent_widget_id IS NULL")
-		@site         = SitemapSetting.find(:first)
+		@widgets = SitemapWidget.find(:all, :conditions => "parent_widget_id IS NULL")
+		@site = SitemapSetting.find(:first)
 		@static_links = SitemapStaticLink.find(:all)
 		respond_to do |format|
 			format.html
